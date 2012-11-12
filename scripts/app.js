73ea70715fd93766f47a17fe1febd463b56cffcc
ysobj <litailang+ysobj@gmail.com>
2012-11-13 07:38:10 +0900

    とりあえずコンポーネントを追加

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 goog.provide('tasks.App');
+goog.require('tasks.Tasks');
 /** @constructor */
 tasks.App = function(){
+		alert('hoge');
+		var ts = new tasks.Tasks();
+		ts.render(document.body);
 };
 new tasks.App();
---
+++ scripts/tasks.js
@@ -0,0 +1,17 @@
+goog.provide('tasks.Tasks');
+goog.require('goog.ui.Component');
+/** @constructor */
+tasks.Tasks = function(){
+		goog.base(this);
+};
+goog.inherits(tasks.Tasks,goog.ui.Component);
+
+tasks.Tasks.prototype.createDom = function(){
+		var dh = this.getDomHelper();
+		this.setElementInternal(dh.createDom('div','TASKS'));
+};
+tasks.Tasks.prototype.enterDocument = function(){
+		goog.base(this,'enterDocument');
+		this.getHandler().listen(this.getElement(),
+						'click', function(){alert('hoge');});
+};
