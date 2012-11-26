f9747e2648cfe9f48e6bf097aa262cbe306cc6ef
ysobj <litailang+ysobj@gmail.com>
2012-11-26 19:01:32 +0900

    ボタンを追加

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,18 @@
 goog.provide('tasks.App');
 goog.require('tasks.Tasks');
 goog.require('goog.dom');
+goog.require('goog.events.EventHandler');
 /** @constructor */
 tasks.App = function(){
 		var main = goog.dom.getElement('main');
 		var ts = new tasks.Tasks();
 		ts.render(main);
+		var btn = goog.dom.getElement('btn_new');
+		var eh_ = new goog.events.EventHandler();
+		eh_.listen(
+						btn,
+						goog.events.EventType.CLICK,
+						function(){ts.createTask('hoge')}
+			);
 };
 new tasks.App();
--- scripts/tasks.js
+++ scripts/tasks.js
@@ -5,26 +5,13 @@ goog.require('tasks.tasks_template');
 /** @constructor */
 tasks.Tasks = function(){
 		goog.base(this);
+		this.tasks = [];
 };
 goog.inherits(tasks.Tasks,goog.ui.Component);
 
 tasks.Tasks.prototype.createDom = function(){
 		var dh = this.getDomHelper();
 		var el = dh.createDom('div',null,null);
-
-		var tbl = dh.createTable(1,9);
-		dh.setProperties(tbl, {class: 'table table-condensed table-bordered'});
-		/*
-		dh.append(tbl.rows[0].cells[0],'TEST');
-		dh.append(tbl.rows[0].cells[1],'日付');
-		dh.append(tbl.rows[0].cells[2],'Project');
-		dh.append(tbl.rows[0].cells[3],'作業内容');
-		dh.append(tbl.rows[0].cells[4],'見積りH');
-		dh.append(tbl.rows[0].cells[5],'見積りM');
-		dh.append(tbl.rows[0].cells[6],'実績');
-		dh.append(tbl.rows[0].cells[7],'開始');
-		dh.append(tbl.rows[0].cells[8],'終了');
-		*/
 		el.appendChild(goog.soy.renderAsElement(tasks.tasks_template.base));
 		this.setElementInternal(el);
 };
@@ -32,4 +19,7 @@ tasks.Tasks.prototype.enterDocument = function(){
 		goog.base(this,'enterDocument');
 		this.getHandler().listen(this.getElement(),
 						'click', function(){alert('hoge');});
+};
+tasks.Tasks.prototype.createTask = function(task){
+	alert('task add');	
 };
